3145632370d599d6c089926f88d1b42a25323d90
RoanKanninga <roankanninga@gmail.com>
2016-07-28 16:04:26 +0200

    added rawdata + results for the test

---
+++ test/test_pipeline.sh
@@ -0,0 +1,35 @@
+workfolder="/groups/umcg-gaf/tmp04/"
+prmfolder="/groups/umcg-gaf/prm02/"
+
+cd ${workfolder}/tmp/
+if [ -d ${workfolder}/tmp/NGS_DNA ]
+then
+	rm -rf ${workfolder}/tmp/NGS_DNA/
+fi
+
+git clone https://github.com/molgenis/NGS_DNA.git
+cd ${workfolder}/tmp/NGS_DNA
+
+if [ ! -d ${workfolder}/rawdata/ngs/MY_TEST_BAM_PROJECT/ ] 
+then
+	cp -r test/rawdata/MY_TEST_BAM_PROJECT/ ${workfolder}/rawdata/ngs/
+fi
+
+if [ -d ${workfolder}/generatedscripts/PlatinumSubset ] 
+then
+	rm -rf ${workfolder}/generatedscripts/PlatinumSubset/
+	mkdir ${workfolder}/generatedscripts/PlatinumSubset/
+fi
+
+rm -f ${workfolder}/logs/PlatinumSubset.pipeline.finished
+
+cp autotest_generate_template.sh ${workfolder}/generatedscripts/PlatinumSubset/generate_template.sh
+
+cd ${workfolder}/generatedscripts/PlatinumSubset/
+
+sh generate_template.sh 
+
+cd scripts
+
+sh submit.sh >> ${workfolder}/generatedscripts/PlatinumSubset/logger.txt 2>&1 
+
