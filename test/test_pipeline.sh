33491546d5a33123dab535a333a1993da929e2a0
RoanKanninga <roankanninga@gmail.com>
2016-08-15 10:39:32 +0200

    edit tests

--- test/test_pipeline.sh
+++ test/test_pipeline.sh
@@ -20,7 +20,7 @@ cd ${workfolder}/tmp/NGS_DNA
 
 git fetch --tags --progress https://github.com/molgenis/NGS_DNA/ +refs/pull/*:refs/remotes/origin/pr/*
 COMMIT=$(git rev-parse refs/remotes/origin/pr/$PULLREQUEST/merge^{commit})
-git config core.sparsecheckout
+echo "checkout commit: COMMIT"
 git checkout -f ${COMMIT}
 
 if [ ! -d ${workfolder}/rawdata/ngs/MY_TEST_BAM_PROJECT/ ] 
@@ -50,6 +50,7 @@ cd ${workfolder}/generatedscripts/PlatinumSubset/
 sh generate_template.sh 
 
 cd scripts
+perl -pi -e 's|module load test|EBROOTNGS_DNA=/groups/umcg-gaf/tmp04/tmp/NGS_DNA/|' *.sh  
 
 sh submit.sh
 
