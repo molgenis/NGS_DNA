33525a259a8d013d3a51f6f2ac125347f9e75263
RoanKanninga <roankanninga@gmail.com>
2016-08-19 09:23:05 +0200

    test

---
+++ protocols/PrepareReAnalysis.sh
@@ -0,0 +1,106 @@
+#MOLGENIS walltime=01:59:00 mem=4gb
+#string tmpName
+#list seqType
+#string project
+#string projectJobsDir
+#string projectLogsDir
+#string intermediateDir
+#string projectResultsDir
+#string batchIDList
+#string projectQcDir
+#string computeVersion
+#string group_parameters
+#string groupname
+#string previousRun
+#string permanentDataDir
+
+#list sequencingStartDate
+#list sequencer
+#list run
+#list flowcell
+#list barcode
+#list lane
+#list internalSampleID
+
+#string mainParameters
+#string worksheet 
+#string outputdir
+#string workflowpath
+#string environment_parameters
+#string tmpdir_parameters
+#string ngsversion
+#string ngsUtilsVersion
+
+#string project
+#string logsDir 
+
+umask 0007
+
+module list
+module load Molgenis-Compute/${computeVersion}
+module load $ngsversion
+#
+# Create project dirs.
+#
+mkdir -p ${projectJobsDir}
+mkdir -p ${projectLogsDir}
+mkdir -p ${intermediateDir}
+mkdir -p ${projectResultsDir}
+mkdir -p ${projectQcDir}
+
+ROCKETPOINT=`pwd`
+
+#
+##
+### Copying prm results data from the run that needs to be reanalyzed
+##
+#
+printf "Copying ${permanentDataDir}/projects/${project}/${previousRun}/results/alignment/* to ${intermediateDir} .."
+rsync -a ${permanentDataDir}/projects/${project}/${previousRun}/results/alignment/* ${intermediateDir}
+printf ".. finished\nCopying ${permanentDataDir}/projects/${project}/${previousRun}/results/qc/*.zip to ${intermediateDir} .."
+rsync -a ${permanentDataDir}/projects/${project}/${previousRun}/results/qc/*.zip ${intermediateDir}
+printf ".. finished\nCopying ${permanentDataDir}/projects/${project}/${previousRun}/results/qc/statistics/* to ${intermediateDir} .."
+rsync -a ${permanentDataDir}/projects/${project}/${previousRun}/results/qc/statistics/* ${intermediateDir}
+printf ".. finished\nCopying ${permanentDataDir}/projects/${project}/${previousRun}/results/general/*chosenSex.txt to ${intermediateDir} .."
+rsync -a ${permanentDataDir}/projects/${project}/${previousRun}/results/general/*chosenSex.txt ${intermediateDir}
+printf "..finished \n"
+
+cd $ROCKETPOINT
+
+echo "before splitting"
+echo `pwd`
+module load ${ngsUtilsVersion}
+module load ${ngsversion}
+#
+# TODO: array for each sample:
+#
+
+#
+# Create subset of samples for this project.
+#
+
+extract_samples_from_GAF_list.pl --i ${worksheet} --o ${projectJobsDir}/${project}.csv --c project --q ${project}
+
+#
+# Execute MOLGENIS/compute to create job scripts to analyse this project.
+#
+
+if [ -f .compute.properties ];
+then
+     rm ../.compute.properties
+fi
+
+echo "before run second rocket"
+echo pwd
+
+sh ${EBROOTMOLGENISMINCOMPUTE}/molgenis_compute.sh -p ${mainParameters} \
+-p ${batchIDList} -p ${projectJobsDir}/${project}.csv -p ${environment_parameters} -p ${group_parameters} -p ${tmpdir_parameters} -rundir ${projectJobsDir} \
+--header ${EBROOTNGS_DNA}/templates/slurm/header.ftl \
+--footer ${EBROOTNGS_DNA}/templates/slurm/footer.ftl \
+--submit ${EBROOTNGS_DNA}/templates/slurm/submit.ftl \
+-w ${workflowpath} \
+-b slurm \
+-g -weave \
+-runid ${runid} \
+-o "ngsversion=${ngsversion};\
+groupname=${groupname}"
