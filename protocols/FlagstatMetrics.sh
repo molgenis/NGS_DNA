f80d55e708a024019a7d3829718185b15a2aa933
RoanKanninga <roankanninga@gmail.com>
2016-08-26 15:15:37 +0200

    bugfix in FlagstatMetrics

--- protocols/FlagstatMetrics.sh
+++ protocols/FlagstatMetrics.sh
@@ -23,16 +23,22 @@ sleep 5
 makeTmpDir ${flagstatMetrics}
 tmpFlagstatMetrics=${MC_tmpFile}
 
+makeTmpDir ${dedupMetrics}
+tmpDedupMetrics=${MC_tmpFile}
+
 echo "starting to calculate flagstat metrics"
 #make metrics file
 ${EBROOTSAMBAMBA}/${sambambaTool} \
 flagstat \
 --nthreads=4 \
-${dedupBam} > ${tmpFlagstatMetrics}.tmp
+${dedupBam} > ${tmpFlagstatMetrics}
 
-echo -e "READ_PAIR_DUPLICATES\tPERCENT_DUPLICATION" > ${tmpFlagstatMetrics}
-sed -n '1p;4p' ${tmpFlagstatMetrics}.tmp | awk '{print $1}' | perl -wpe 's|\n|\t|' | awk '{print $2"\t"($2/$1)*100}' >> ${tmpFlagstatMetrics}
-
-echo -e "\nFlagstatMetrics calculated. Moving temp files to final.\n\n"
 mv ${tmpFlagstatMetrics} ${flagstatMetrics}
 echo "moved ${tmpFlagstatMetrics} ${flagstatMetrics}"
+
+echo -e "READ_PAIR_DUPLICATES\tPERCENT_DUPLICATION" > ${tmpDedupMetrics}
+sed -n '1p;4p' ${flagstatMetrics} | awk '{print $1}' | perl -wpe 's|\n|\t|' | awk '{print $2"\t"($2/$1)*100}' >> ${tmpDedupMetrics}
+
+echo -e "\nFlagstatMetrics calculated. Moving temp files to final.\n\n"
+mv ${tmpDedupMetrics} ${dedupMetrics}
+echo "moved ${tmpDedupMetrics} ${dedupMetrics}"
