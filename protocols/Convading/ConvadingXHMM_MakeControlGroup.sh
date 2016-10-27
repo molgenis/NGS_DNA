dc69976d6d7bb860101f27b3c8807ba58e09858c
RoanKanninga <roankanninga@gmail.com>
2016-10-27 12:31:01 +0200

    make controlgroup script

--- protocols/Convading/ConvadingXHMM_MakeControlGroup.sh
+++ protocols/Convading/ConvadingXHMM_MakeControlGroup.sh
@@ -1,15 +1,10 @@
 set -e 
 set -u
 
-module load CoNVaDING/1.1.6
-module load GATK
 function usage () {
 echo "
 
 sh Convading_MakeControlGroup.sh -i /path/to/bams -w /path/to/workdir/ -p ONCO_v3 -b /apps/data/Agilent/ONCO_v3/human_g1k_v37/captured.bed
-
-There are three layers of data: workDir, ControlsDir/version and ControlsDir with symlinks to specific version
-
 
 Arguments
         Required:
@@ -19,12 +14,12 @@ Arguments
 	-b|--bedfile		full path to bedfile (e.g. /apps/data/Agilent/ONCO_v3/human_g1k_v37/captured.bed)
 	Optional:
 	-o|--controlsDir	Place where final controls will be, the name of the panel will be pasted as folder and MONTH 
-				default: /apps/data/ConvadingXHMM/\${panel}/\${MONTH}
+				default: /apps/data/Controls_Convading_XHMM/${panel}/\${MONTH}
 	-r|--reference		path to reference genome (e.g. /apps/data/1000G/phase1/human_g1k_v37_phiX.fasta)
 				default: /apps/data/1000G/phase1/human_g1k_v37_phiX.fasta"
 }
 
-module load ngs-utils
+
 PARSED_OPTIONS=$(getopt -n "$0"  -o i:w:p:b:o:r: --long "bamsfolder:workdir:panel:bedfile:controlsDir:reference:"  -- "$@")
 
 #
@@ -96,12 +91,15 @@ if [[ -z "${capturedBed-}" ]]; then
 fi
 
 if [[ -z "${pathToControls-}" ]]; then
-        pathToControls="/apps/data/ConvadingXHMM/"
+        pathToControls="/apps/data/Controls_Convading_XHMM/"
 fi
 if [[ -z "${indexFile-}" ]]; then
         indexFile="/apps/data/1000G/phase1/human_g1k_v37_phiX.fasta"
 fi
 
+module load ngs-utils
+module load CoNVaDING/1.1.6
+module load GATK
 
 
 cd $bamsFolder
@@ -128,7 +126,6 @@ startWithBamFinished="${workingDir}/step1_startWithBam.finished"
 startWithMatchScoreFinished="${workingDir}/step2_startWithMatchScore.finished"
 startWithBestScoreFinished="${workingDir}/step3_startWithBestScore.finished"
 generateTargetQcListFinished="${workingDir}/step4_generateTargetQcList.finished"
-createFinalListFinished="${workingDir}/step5_createFinalList.finished"
 
 if [ ! -f ${startWithBamFinished} ]
 then
@@ -179,68 +176,37 @@ else
 	echo "startWithBestScore skipped"
 fi
 
-if [ ! -f ${generateTargetQcListFinished} ]
-then
-	echo "working on: s4_generateTargetQcList"
-	perl ${EBROOTCONVADING}/CoNVaDING.pl \
-	-mode GenerateTargetQcList \
-	-inputDir ${controlsDir} \
-	-outputDir ${convadingGenerateTargetQcListDir} \
-	-controlsDir ${controlsDir} >> step4_generateTargetQcList.log
-	
-	touch ${generateTargetQcListFinished}
-	echo "generateTargetQcList created"
-else
-	echo "GenerateTargetQcList skipped"
-fi
-
-if [ ! -f ${createFinalListFinished} ]
-then
-	echo "working on: s5_CreateFinalList"
-	perl ${EBROOTCONVADING}/CoNVaDING.pl \
-	-mode CreateFinalList \
-	-inputDir ${convadingStartWithBestScoreDir} \
-	-outputDir ${convadingCreateFinalListDir} \
-	-targetQcList ${convadingGenerateTargetQcListDir}/targetQcList.txt >> step5_createFinalList.log
-	
-	touch ${createFinalListFinished}
-	echo "createFinalList created"	
-else
-	echo "CreateFinalList skipped"
-fi
-
+#
+##Step 4: PICK OUT BAD SAMPLES
+#
 mkdir -p ${kickedOutSamples}
-rm -f step6_removingSamplesFromControls.log 
-
-for i in $(ls ${convadingCreateFinalListDir}/*)
-do
-	size=$(cat $i | wc -l)
-	if [ $size != 1 ]
-	then
-		
-		T=$(basename ${i%%.*})
-		if [ -f ${controlsDir}/${T}*.aligned.only.normalized.coverage.txt ]
-		then
-			mv ${controlsDir}/${T}*.aligned.only.normalized.coverage.txt ${kickedOutSamples}/
-			printf "number of outliers: ${size}\t${controlsDir}/${T}*.aligned.only.normalized.coverage.txt moved to ${kickedOutSamples} folder \n" >> step6_removingSamplesFromControls.log
-	                printf "You can check the outliers in: $i \n\n" | tee step6_removingSamplesFromControls.log
-		fi
-		rm ${convadingInputBamsDir}/${T}*
-
-
-	fi
-done
-
-echo "Convading is completed, the controls with outliers has been removed"
-
+rm -f step4_removingSamplesFromControls.log 
+echo "removing bad samples out of the controlsDir"
 version=$(date +"%Y-%m")
+
+for i in $(ls ${convadingStartWithBestScoreDir}/*.shortlist.txt)
+do
+##SHORTLIST (uit stap3)
+        size=$(cat $i | wc -l)
+        if [ $size != 1 ]
+        then
+            	T=$(basename ${i%%.*})
+                if [ -f ${controlsDir}/${T}*.shortlist.txt ]
+                then
+                    	mv ${controlsDir}/${T}*.aligned.only.normalized.coverage.txt ${kickedOutSamples}/
+                        printf "number of outliers: ${size}\t${controlsDir}/${T}*.aligned.only.normalized.coverage.txt moved to ${kickedOutSamples} folder \n" >> step4_removingSamplesFromControls.log
+                        printf "You can check the outliers in: $i \n\n" | tee step4_removingSamplesFromControls.log
+                fi
+                rm ${convadingInputBamsDir}/${T}*
+
+        fi
+done
 
 pathToFinalControls="${pathToControls}/${panel}/"
 mkdir -p ${pathToFinalControls}/${version}/bams/
 mkdir -p ${pathToFinalControls}/${version}/Convading/
 mkdir -p ${pathToFinalControls}/${version}/XHMM/
 
-###COPYING Convading controls normalized coverage files to final destination
 echo "###COPYING Convading controls normalized coverage files to final destination"
 
 cp ${controlsDir}/*.aligned.only.normalized.coverage.txt ${pathToFinalControls}/${version}/Convading
@@ -248,34 +214,46 @@ echo "copied ${controlsDir}/${T}*.aligned.only.normalized.coverage.txt to ${path
 
 ##Make symlinks to bams
 echo "make symlinks to bams"
-cp -a ${convadingInputBamsDir}/* ${pathToFinalControls}/${version}/bams
+cp -a ${convadingInputBamsDir}/*.bam* ${pathToFinalControls}/${version}/bams/
+
+
+if [ ! -f ${generateTargetQcListFinished} ]
+then
+	echo "working on: s4_generateTargetQcList with updated controls list"
+	perl ${EBROOTCONVADING}/CoNVaDING.pl \
+	-mode GenerateTargetQcList \
+	-inputDir ${pathToFinalControls}/${version}/Convading \
+	-outputDir ${convadingGenerateTargetQcListDir} \
+	-controlsDir ${pathToFinalControls}/${version}/Convading >> step5_generateTargetQcList.log
+	
+	touch ${generateTargetQcListFinished}
+	echo "generateTargetQcList created"
+else
+	echo "GenerateTargetQcList skipped"
+fi
+
 
 mkdir -p ${workingDir}/XHMM
-if [ ! -f ${workingDir}/xhmm.finished ]
+if [ ! -f ${workingDir}/XHMM.finished ]
 then 
-	rm -f ${workingDir}/XHMM/${panel}.READS.bam.list
-	groupName=1
-	teller=1
+
 	for i in $(ls ${pathToFinalControls}/${version}/bams/*.bam)
 	do
-	  	echo "$i" >> ${workingDir}/XHMM/group${groupName}.READS.bam.list
-		teller=$((teller+1))
-		if [ $teller == 15 ]
-		then
-			groupName=$((groupName+1))
-			teller=1
-		fi
+		name=$(basename ${i%%.*})
+	  	echo "$i" > ${workingDir}/XHMM/${name}.READS.bam.list
+
 	done
 	
-	for i in $(ls ${workingDir}/XHMM/group*.READS.bam.list)
+	for i in $(ls ${workingDir}/XHMM/*.READS.bam.list)
 	do
 	mkdir -p ${workingDir}/XHMM/scripts/
 	bas=$(basename ${i%%.*})
 	output=${workingDir}/XHMM/scripts/xhmm_${bas}.sh
+
 	echo "#!/bin/bash" > ${output}
 	echo "#SBATCH --job-name=xhmm_${bas}" >> ${output}
-	echo "#SBATCH --output=xhmm_${bas}.out" >> ${output}
-	echo "#SBATCH --error=xhmm_${bas}_0.err" >> ${output}
+	echo "#SBATCH --output=${workingDir}/XHMM/scripts/xhmm_${bas}.out" >> ${output}
+	echo "#SBATCH --error=${workingDir}/XHMM/scripts/xhmm_${bas}.err" >> ${output}
 	echo "#SBATCH --time=05:59:00" >> ${output}
 	echo "#SBATCH --cpus-per-task 1" >> ${output}
 	echo "#SBATCH --mem 6gb" >> ${output}
@@ -284,6 +262,11 @@ then
 	echo "#SBATCH --get-user-env=30L" >> ${output}
 	echo "set -e" >> ${output}
 	echo "set -u" >> ${output}
+	echo "function finish {">> ${output}
+        echo "echo \"TRAPPED\"">> ${output}
+        echo "touch ${workingDir}/XHMM/scripts/XHMM.failed">> ${output}
+        echo "}">> ${output}
+	echo "trap finish HUP INT QUIT TERM EXIT ERR" >> ${output}
 	echo "module load GATK" >> ${output}
 	echo "java -Xmx5g -jar ${EBROOTGATK}/GenomeAnalysisTK.jar \\" >> ${output}
 	echo "-T DepthOfCoverage \\">> ${output}
@@ -294,17 +277,57 @@ then
 	echo "--minBaseQuality 0 --minMappingQuality 20 --start 1 --stop 5000 --nBins 200 \\">> ${output}
 	echo "--includeRefNSites \\">> ${output}
 	echo "--countType COUNT_FRAGMENTS \\" >> ${output}
-	echo "-o ${workingDir}/XHMM/" >> ${output}
-
-	sbatch $output
-	echo "sbatch ${output}"
+	echo "-o ${workingDir}/XHMM/${bas}" >> ${output}
+	echo -e "\ntouch ${output}.finished" >> ${output}
+	echo -e "\ntrap - EXIT">> ${output}
+	echo "exit 0">> ${output}
+	
+	if [ ! -f ${output}.finished ]
+	then
+		sbatch $output
+	else
+		echo "${output} skipped"
+	fi
 	done
 
-	touch ${workingDir}/xhmm.finished
+	touch ${workingDir}/XHMM.submitted
 fi
-### COPYING XHMM controls to final destination
-echo "### COPYING XHMM controls to final destination"
 
-cp -r ${workingDir}/XHMM/* ${pathToFinalControls}/${version}/XHMM/
-echo "copied ${workingDir}/XHMM/ to ${pathToFinalControls}/${version}/XHMM"
+while [[ ! -f ${workingDir}/XHMM.finished && ! -f ${workingDir}/XHMM/scripts/XHMM.failed ]]
+do
+	
+	countSh=$(ls ${workingDir}/XHMM/scripts/*.sh | wc -l)
+	countFinished=0
+	if ls ${workingDir}/XHMM/scripts/*.sh.finished 1> /dev/null 2>&1
+	then
+		countFinished=$(ls ${workingDir}/XHMM/scripts/*.sh.finished | wc -l)
+	fi
+
+	if [ $countSh == $countFinished ]
+	then
+		echo "${countFinished} scripts finished out of the ${countSh}"
+		echo "FINISHED!!!"
+		touch ${workingDir}/XHMM.finished	
+	else
+		echo "${countFinished} scripts finished out of the ${countSh}"
+		echo "XHMM is not finished yet, going to sleep for 1 minute"
+
+		sleep 60
+	fi
+done
+
+if [ -f ${workingDir}/XHMM/scripts/XHMM.failed ]
+then
+	echo "An error has occured, please check the .err files in ${workingDir}/XHMM/scripts/"
+	exit 1
+fi
+
+if [ -f ${workingDir}/XHMM.finished ]
+then
+	echo "### COPYING XHMM controls to final destination"
+
+	cp -r ${workingDir}/XHMM/*.sample_interval_summary ${pathToFinalControls}/${version}/XHMM/
+	echo "xhmm results copied"
+	
+fi
 
