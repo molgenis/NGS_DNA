bc8e3728d603fff6acf3a17fe35a1684c2b538ba
RoanKanninga <roankanninga@gmail.com>
2016-11-28 11:06:35 +0100

    fix gender check

--- gender.sh
+++ gender.sh
@@ -1,4 +1,5 @@
-HEADER=$(head -1 $1)
+samplesheetFile=$1
+HEADER=$(head -1 ${samplesheetFile})
 OLDIFS=$IFS
 IFS=','
 array=($HEADER)
@@ -13,21 +14,23 @@ do
         fi
 done
 
-echo $genderCheck
+echo "GenderCheck is $genderCheck"
 
 if [ "${genderCheck}" == "false" ] 
 then	
+	echo "Adding Gender column and values to ${samplesheetFile}, writing to ${samplesheetFile}.tmp"
 	awk '{
 	if (NR==1){
 		print $0",gender"
 	}
 	else{
 		print $0",Unknown"
-	}}' $1
-fi $1 >> $1.tmp
+	}}' ${samplesheetFile} >> ${samplesheetFile}.tmp
+fi 
 
 if [ "${genderCheck}" == "false" ]
 then
 	echo "Samplesheet has been updated, there is a gender column added with all Unknown as value for each row" > ./samplesheetHasbeenUpdated.txt
+	echo "moving ${samplesheetFile}.tmp to ${samplesheetFile}"
 	mv $1.tmp $1
 fi
