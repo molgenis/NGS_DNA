84bdcace412291e40fae7ed67482f5056af475c5
RoanKanninga <roankanninga@gmail.com>
2017-05-04 16:18:51 +0200

    fix test results

--- test/protocols/Autotest.sh
+++ test/protocols/Autotest.sh
@@ -7,21 +7,33 @@
 #string projectResultsDir
 #string ngsUtilsVersion
 
-rm -rf /home/umcg-molgenis/NGS_DNA/output
-
 module load ngs-utils
 
-${EBROOTNGSMINUTILS}/vcf-compare_2.0.sh -1 /home/umcg-molgenis/NGS_DNA/PlatinumSample_True.final.vcf.gz -2 ${projectResultsDir}/variants/PlatinumSample.final.vcf.gz -o /home/umcg-molgenis/NGS_DNA/output
+rm -rf /home/umcg-molgenis/NGS_DNA/output
+count=0
+for i in PlatinumSubset PlatinumSample_NA12878 PlatinumSample_NA12891
+do
+	mkdir -p /home/umcg-molgenis/NGS_DNA/output/${i}
+	
+	${EBROOTNGSMINUTILS}/vcf-compare_2.0.sh -1 /home/umcg-molgenis/NGS_DNA/${i}_True.final.vcf.gz -2 ${projectResultsDir}/variants/${i}.final.vcf.gz -o /home/umcg-molgenis/NGS_DNA/output/${i}/
+	
+	if [[ -f /home/umcg-molgenis/NGS_DNA/output/${i}/notInVcf1.txt || -f /home/umcg-molgenis/NGS_DNA/output/${i}/notInVcf2.txt || -f /home/umcg-molgenis/NGS_DNA/output/${i}/inconsistent.txt ]]
+	then
+		echo "${i}: there are differences between the test and the original output"
+        	echo "please fix the bug or update this test"
+        	echo "the stats can be found here: /home/umcg-molgenis/NGS_DNA/output/${i}/vcfStats.txt"
+        	exit 1
+	else
+		echo "${i}: test succeeded"
+		head -2 /home/umcg-molgenis/NGS_DNA/output/${i}/vcfStats.txt	
+		count=$((count + 1)
+	fi
+done
 
-
-if [[ -f /home/umcg-molgenis/NGS_DNA/output/notInVcf1.txt || -f /home/umcg-molgenis/NGS_DNA/output/notInVcf2.txt || -f /home/umcg-molgenis/NGS_DNA/output/inconsistent.txt ]]
+if [ ${count} == "3" ]
 then
-	echo "there are differences between the test and the original output"
-        echo "please fix the bug or update this test"
-        echo "the stats can be found here: /home/umcg-molgenis/NGS_DNA/output/vcfStats.txt"
-        exit 1
+	echo "Autotest complete"
+	echo "the statistics of the 3 tests can be found here: /home/umcg-molgenis/NGS_DNA/output/"
 else
-	echo "test succeeded"
-	head -2 /home/umcg-molgenis/NGS_DNA/output/vcfStats.txt
-
+	echo "interesting, this cannot be true, count is ${count}"		
 fi
