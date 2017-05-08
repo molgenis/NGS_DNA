7fba9ccf392c6982e499c3545e63db073fe5fbdb
RoanKanninga <roankanninga@gmail.com>
2017-05-08 16:33:11 +0200

    fixing PhaseByTransmission for GoNL quattro families

--- protocols/GoNL/PhaseByTransmission.sh
+++ protocols/GoNL/PhaseByTransmission.sh
@@ -11,7 +11,7 @@
 #string projectBatchGenotypedVariantCalls
 
 ml $gatkVersion
- 
+echo "round 1, abc samples" 
 if [ -f "${projectBatchGenotypedVariantCalls}" ]
 then
 java -XX:ParallelGCThreads=2 -Djava.io.tmpdir=${tempDir} -Xmx12g -jar ${EBROOTGATK}/${gatkJar} \
@@ -20,6 +20,48 @@ java -XX:ParallelGCThreads=2 -Djava.io.tmpdir=${tempDir} -Xmx12g -jar ${EBROOTGA
    -V "${projectBatchGenotypedVariantCalls}" \
    -mvf "${projectBatchGenotypedVariantCalls}.mvf" \
    -ped ${intermediateDir}/children.ped \
-   -o "${projectBatchGenotypedVariantCalls}.PBToutput.vcf"
-
+   -o "${projectBatchGenotypedVariantCalls}.PBToutput.abc.vcf"
 fi
+
+echo "round 1, exclude d samples" 
+
+##exclude d samples
+java -XX:ParallelGCThreads=2 -Djava.io.tmpdir=${tempDir} -Xmx12g -jar ${EBROOTGATK}/${gatkJar} \
+-T SelectVariants \
+-R ${indexFile} \
+--variant ${projectBatchGenotypedVariantCalls}.PBToutput.abc.vcf \
+-o ${projectBatchGenotypedVariantCalls}.PBToutput.vcf.abcFiltered.vcf \
+-xl_se 'gonl-.+d'
+
+echo "round 2, abd samples"
+if [ -f "${projectBatchGenotypedVariantCalls}" ]
+then
+java -XX:ParallelGCThreads=2 -Djava.io.tmpdir=${tempDir} -Xmx12g -jar ${EBROOTGATK}/${gatkJar} \
+   -T PhaseByTransmission \
+   -R ${indexFile} \
+   -V "${projectBatchGenotypedVariantCalls}" \
+   -mvf "${projectBatchGenotypedVariantCalls}.abd.mvf" \
+   -ped ${intermediateDir}/children_d.ped \
+   -o "${projectBatchGenotypedVariantCalls}.PBToutput.abd.vcf"
+fi
+
+echo "round 2, include d samples"
+##include only d samples
+java -XX:ParallelGCThreads=2 -Djava.io.tmpdir=${tempDir} -Xmx12g -jar ${EBROOTGATK}/${gatkJar} \
+-T SelectVariants \
+-R ${indexFile} \
+--variant ${projectBatchGenotypedVariantCalls}.PBToutput.abd.vcf \
+-o ${projectBatchGenotypedVariantCalls}.PBToutput.vcf.dFiltered.vcf \
+-se 'gonl-.+d'
+
+echo "make one big vcf containing abc and d samples" 
+## make one big vcf containing abc and d samples
+java -XX:ParallelGCThreads=2 -Djava.io.tmpdir=${tempDir} -Xmx12g -jar ${EBROOTGATK}/${gatkJar} \
+-T CombineVariants \
+-R ${indexFile}  \
+   --variant ${projectBatchGenotypedVariantCalls}.PBToutput.vcf.abcFiltered.vcf \
+   --variant ${projectBatchGenotypedVariantCalls}.PBToutput.vcf.dFiltered.vcf \
+   -o ${projectBatchGenotypedVariantCalls}.PBToutput.vcf \
+   -genotypeMergeOptions UNIQUIFY
+
+
