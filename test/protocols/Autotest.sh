4b8f5ecdb4e0751ecb6e1bb89b847fc4a2126a3f
RoanKanninga <roankanninga@gmail.com>
2017-06-06 14:38:47 +0200

    updated test results (change in ploidy chrY) plus cleanup in Autotest

--- test/protocols/Autotest.sh
+++ test/protocols/Autotest.sh
@@ -14,18 +14,17 @@ count=0
 for i in PlatinumSubset PlatinumSample_NA12878 PlatinumSample_NA12891
 do
 	mkdir -p /home/umcg-molgenis/NGS_DNA/output/${i}
-	
 	${EBROOTNGSMINUTILS}/vcf-compare_2.0.sh -1 /home/umcg-molgenis/NGS_DNA/${i}_True.final.vcf.gz -2 ${projectResultsDir}/variants/${i}.final.vcf.gz -o /home/umcg-molgenis/NGS_DNA/output/${i}/
-	
+
 	if [[ -f /home/umcg-molgenis/NGS_DNA/output/${i}/notInVcf1.txt || -f /home/umcg-molgenis/NGS_DNA/output/${i}/notInVcf2.txt || -f /home/umcg-molgenis/NGS_DNA/output/${i}/inconsistent.txt ]]
 	then
 		echo "${i}: there are differences between the test and the original output"
-        	echo "please fix the bug or update this test"
-        	echo "the stats can be found here: /home/umcg-molgenis/NGS_DNA/output/${i}/vcfStats.txt"
-        	exit 1
+		echo "please fix the bug or update this test"
+		echo "the stats can be found here: /home/umcg-molgenis/NGS_DNA/output/${i}/vcfStats.txt"
+		exit 1
 	else
 		echo "${i}: test succeeded"
-		head -2 /home/umcg-molgenis/NGS_DNA/output/${i}/vcfStats.txt	
+		head -2 /home/umcg-molgenis/NGS_DNA/output/${i}/vcfStats.txt
 		count=$((count + 1))
 	fi
 done
@@ -35,5 +34,5 @@ then
 	echo "Autotest complete"
 	echo "the statistics of the 3 tests can be found here: /home/umcg-molgenis/NGS_DNA/output/"
 else
-	echo "interesting, this cannot be true, count is ${count}"		
+	echo "interesting, this cannot be true, count is ${count}"
 fi
