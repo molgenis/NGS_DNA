427c8ea70500f390788eaaf05dec30bfeae55cbe
RoanKanninga <roankanninga@gmail.com>
2017-06-30 08:16:59 +0200

    changing output in PrepareFastQ step since it has storage issues when deploying multiple big jobs

--- protocols/PrepareFastQ.sh
+++ protocols/PrepareFastQ.sh
@@ -70,79 +70,81 @@ fi
 echo -e "finished with phiX part...\nstarting with IlluminaEncoding"
 
 checkIlluminaEncoding() {
-barcodeFqGz=$1
-barcodeFinalFqGz=$2
+	barcodeFqGz=$1
+	barcodeFinalFqGz=$2
 
-lines=($(zcat ${barcodeFqGz} | head -8000 | tail -192 | awk 'NR % 4 == 0'))
-count=1
-nodecision=0
-numberoflines=0
-for line in  ${lines[@]}
-do
-	numberoflines=$(( numberoflines+1 ))
-	#check for illumina encoding 1.5
-	if [[ "$line" =~ [P-Z] ]] || [[ "$line" =~ [a-g] ]]
+	lines=($(zcat ${barcodeFqGz} | head -8000 | tail -192 | awk 'NR % 4 == 0'))
+	count=1
+	nodecision=0
+	numberoflines=0
+	for line in ${lines[@]}
+	do
+		numberoflines=$(( numberoflines+1 ))
+		#check for illumina encoding 1.5
+		if [[ "$line" =~ [P-Z] ]] || [[ "$line" =~ [a-g] ]]
+			then
+				encoding="1.5
+			if [[ ${count} -eq 1 ]]
+			then
+				lastEncoding=${encoding}
+				count=$(( count+1 ))
+			fi
+
+			if ! [ "${encoding}" == "${lastEncoding}" ]
+			then
+				echo "error, encoding not possible"
+				echo "${encoding} is not matching last encoding (${lastEncoding}"
+				echo "LINE: " $line
+				exit 1
+			fi
+			lastEncoding=${encoding}
+
+		#check for illumina encoding 1.8/1.9
+		elif [[ "$line" =~ [0-9] ]] || [[ "$line" =~ [\<=\>?] ]]
 		then
-        	encoding="1.5"
-	        if [[ ${count} -eq 1 ]]
-        	then
-            	lastEncoding=${encoding}
-            	count=$(( count+1 ))
-        	fi
-
-        	if ! [ "${encoding}" == "${lastEncoding}" ]
-        	then
-	            	echo "error, encoding not possible"
-			echo "${encoding} is not matching last encoding (${lastEncoding}"
-            		echo "LINE: " $line
+			encoding="1.9"
+			if [[ ${count} -eq 1 ]]
+			then
+				lastEncoding=${encoding}
+				count=$(( count+1 ))
+			fi
+			if ! [ "${encoding}" == "${lastEncoding}" ]
+			then
+				echo "error, encoding not possible"
+				echo "${encoding} is not matching last encoding (${lastEncoding}"
+				echo "LINE: " $line
 			exit 1
-        	fi
-        	lastEncoding=${encoding}
-
-	#check for illumina encoding 1.8/1.9
-	elif [[ "$line" =~ [0-9] ]] || [[ "$line" =~ [\<=\>?] ]]
-     	then
-        	encoding="1.9"
-	        if [[ ${count} -eq 1 ]]
-        	then
-        		lastEncoding=${encoding}
-	        	count=$(( count+1 ))
-        	fi
-        	if ! [ "${encoding}" == "${lastEncoding}" ]
-        	then
-                	echo "error, encoding not possible"
-			echo "${encoding} is not matching last encoding (${lastEncoding}"
-			echo "LINE: " $line
-	                exit 1
-                fi
-              	lastEncoding="${encoding}"
-	elif [[ "$line" =~ @ ]] || [[ "$line" =~ [A-J] ]]
-        	then
-                nodecision=$(( nodecision+1 ))
-	else
-		echo "The encoding is not matching to anything, check FastQ documentation (count=$count)"
+			fi
+			lastEncoding="${encoding}"
+		elif [[ "$line" =~ @ ]] || [[ "$line" =~ [A-J] ]]
+		then
+			nodecision=$(( nodecision+1 ))
+		else
+			echo "The encoding is not matching to anything, check FastQ documentation (count=$count)"
+		fi
+	done
+	if [ "${nodecision}" == "${numberoflines}" ]
+	then
+		echo "Within all the lines, no decision was made about the encoding, all the encoding is between A and J. This is then probably an 1.9 encoding sample, so 1.9 is set as encoding"
+		encoding="1.9"
 	fi
-done
-if [ "${nodecision}" == "${numberoflines}" ]
-then
-	echo "Within all the lines, no decision was made about the encoding, all the encoding is between A and J. This is then probably an 1.9 encoding sample, so 1.9 is set as encoding"
-	encoding="1.9"
-fi
 
-if [ "${encoding}" == "1.9" ]
-then
-	echo "encoding is Illumina 1.8 - Sanger / Illumina 1.9"
-	echo "move ${barcodeFqGz} to ${barcodeFinalFqGz}"
-	mv ${barcodeFqGz} ${barcodeFinalFqGz}
+	if [ "${encoding}" == "1.9" ]
+	then
+		echo "encoding is Illumina 1.8 - Sanger / Illumina 1.9"
+		echo "Only move ${barcodeFqGz} to ${barcodeFinalFqGz}"
+		mv ${barcodeFqGz} > ${barcodeFinalFqGz}
 
-else
-	#make fastQ out of the fq.gz file
-	echo "converting Illumina encoding"
-	seqtk seq ${barcodeFqGz} -Q 64 -V > ${barcodeFqGz}.encoded.fq
-	echo -e "done..\nNow gzipping ${barcodeFqGz}.encoded.fq > ${barcodeFinalFqGz}"
-	pigz -c ${barcodeFqGz}.encoded.fq > ${barcodeFinalFqGz}
+	else
+		#make fastQ out of the fq.gz file
+		mkfifo ${barcodeFq}.encoded.fq
+		echo "converting Illumina encoding"
+		seqtk seq ${barcodeFqGz} -Q 64 -V > ${barcodeFq}.encoded.fq&
 
-fi
+		echo -e "done..\nNow gzipping ${barcodeFq}.encoded.fq > ${barcodeFinalFqGz}"
+		pigz -c ${barcodeFq}.encoded.fq > ${barcodeFinalFqGz}
+
+	fi
 
 }
 
