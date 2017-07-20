7de164760a48038629555bd89073017801cd5857
RoanKanninga <roankanninga@gmail.com>
2017-07-20 09:25:10 +0200

    added M&M part to QCReport

--- protocols/QCReport.sh
+++ protocols/QCReport.sh
@@ -108,6 +108,29 @@ cat > ${intermediateDir}/${project}_QCReport.rhtml <<'_EOF'
 <div class="page" style="text-align:center;">
 <font STYLE="font-size: 45pt;">
 <br>
+Preprocessing:
+<br>During the first preprocessing steps of the pipeline, PhiX reads are inserted in each sample to create control SNPs in the dataset.
+<br>Subsequently, Illumina encoding is checked and QC metrics are calculated using a FastQC tool Andrews S. (2010)<sup>1</sup>
+<br>
+<br>Alignment to a reference genome:
+<br>The bwa-mem command from Burrows-Wheeler Aligner(BWA) (Li & Durbin <sup>2</sup>)  is used to align the sequence data to a reference genome resulting in a SAM (Sequence Alignment Map) file.
+<br>The reads in the SAM file are sorted with Sambamba(Tarasov et al.<sup>3</sup>). resulting in a sorted BAM file. When multiple lanes were used during sequencing,
+<br>all lane BAMs were merged into a sample BAM using Sambamba. The (merged) BAM file is marked for duplicates of the same read pair using Sambamba.
+
+<br>Variant discovery:
+<br>The GATK (McKenna et al. <sup>4</sup>) HaplotypeCaller estimates the most likely genotypes and allele frequencies in an alignment using a Bayesian likelihood model
+<br>for every position of the genome regardless of whether a variant was detected at that site or not. This information can later be used in the project based genotyping step.
+<br>A joint analysis has been performed of all the samples in the project. This leads to a posterior probability of a variant allele at a site. 
+<br>SNPs and small Indels are written to a VCF file, along with information such as genotype quality, allele frequency, strand bias and read depth for that SNP/Indel.
+<br>Based on quality thresholds from the GATK "best practices" (Van der Auwera et al.<sup>5</sup>) the SNPs and indels are filtered and marked as Lowqual or Pass resulting in a final VCF file.
+
+<br>Reference
+<br>1. Andrews S. (2010). FastQC: a quality control tool for high throughput sequence data. Available online at:http://www.bioinformatics.babraham.ac.uk/projects/fastqc
+<br>2. Li Durbin, Fast and accurate short read alignment with Burrows-Wheeler transform.
+<br>3. Sambamba: Fast processing of NGS alignment formats 
+<br>4. The Genome Analysis Toolkit: a MapReduce framework for analyzing next-generation DNA sequencing data
+<br>5. From FastQ data to high confidence variant calls: the Genome Analysis Toolkit best practices pipeline
+
 <br>
 <br>
 <br>
