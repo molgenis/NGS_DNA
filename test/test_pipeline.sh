27ee915f419894891efab83b2f5d962d219679cc
RoanKanninga <roankanninga@gmail.com>
2017-07-20 10:13:41 +0200

    fix test part 4

--- test/test_pipeline.sh
+++ test/test_pipeline.sh
@@ -71,7 +71,7 @@ cd ${workfolder}/generatedscripts/PlatinumSubset/
 sh generate_template.sh 
 
 cd scripts
-perl -pi -e 's|module load \"\$ngsversion\"|EBROOTNGS_DNA=/groups/umcg-gaf/tmp04/tmp/NGS_DNA/\n#string computeVersion\nmodule load Molgenis-Compute/\${computeVersion}|' *.sh
+perl -pi -e 's|module load \"\${ngsversion}\"|EBROOTNGS_DNA=/groups/umcg-gaf/tmp04/tmp/NGS_DNA/\n#string computeVersion\nmodule load Molgenis-Compute/\${computeVersion}|' *.sh
 sh submit.sh
 
 cd ${workfolder}/projects/PlatinumSubset/run01/jobs/
