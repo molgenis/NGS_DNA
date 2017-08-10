c10fd78b009133a5bd632f1cfb0e4d5f7d3d825a
RoanKanninga <roankanninga@gmail.com>
2017-08-10 15:45:49 +0200

    fix

--- test/test_pipeline.sh
+++ test/test_pipeline.sh
@@ -41,7 +41,7 @@ function preparePipeline(){
 	perl -pi -e 's|sh \$EBROOTMOLGENISMINCOMPUTE/molgenis_compute.sh|module load Molgenis-Compute/dummy\nsh \$EBROOTMOLGENISMINCOMPUTE/molgenis_compute.sh|' ${workfolder}/generatedscripts/${_projectName}/generate_template.sh
 	perl -pi -e "s|module load Molgenis-Compute/dummy|module load Molgenis-Compute/\$mcVersion|" ${workfolder}/generatedscripts/${_projectName}/generate_template.sh
 
-	perl -pi -e 's|workflow=\${EBROOTNGS_DNA}/workflow.csv|workflow=${EBROOTNGS_DNA}/test_workflow.csv|" ${workfolder}/generatedscripts/${_projectName}/generate_template.sh
+	perl -pi -e "s|workflow=\${EBROOTNGS_DNA}/workflow.csv|workflow=${EBROOTNGS_DNA}/test_workflow.csv|" ${workfolder}/generatedscripts/${_projectName}/generate_template.sh
 	echo "where am i"
 	pwd
 	cp test/${_projectName}.csv ${workfolder}/generatedscripts/${_projectName}/
