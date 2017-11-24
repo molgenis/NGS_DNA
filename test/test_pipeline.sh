396dc53e527094b3790711f28f29f364188a8902
RoanKanninga <roankanninga@gmail.com>
2017-11-24 15:16:24 +0100

    fix test

--- test/test_pipeline.sh
+++ test/test_pipeline.sh
@@ -59,7 +59,7 @@ function preparePipeline(){
 	cd ${workfolder}/projects/${_projectName}/run01/jobs/
 
 	perl -pi -e 's|--runDir ${tmpMantaDir}|--region 2:100000-500000 \\\n --runDir ${tmpMantaDir}|' s*_Manta_0.sh
-	perl -pi -e "s|module load \"test\"|""|' s*_Manta_0.sh
+	perl -pi -e 's|module load \"test\"||' s*_Manta_0.sh
         perl -pi -e 's|\$\{EBROOTNGS_DNA\}/conf/configManta.py.ini|'${workfolder}'/tmp/NGS_DNA/conf/configManta.py.ini|' s*_Manta_0.sh
 
 	for i in $(ls s*_CoverageCalculations*.sh); do touch $i.finished ; touch ${i%.*}.env; chmod 755 ${i%.*}.env ;done
