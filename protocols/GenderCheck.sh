557b44e2f90cfb58313d6356136bb18136d5d706
RoanKanninga <roankanninga@gmail.com>
2018-03-21 09:20:17 +0100

    added missing quotes

--- protocols/GenderCheck.sh
+++ protocols/GenderCheck.sh
@@ -133,7 +133,7 @@ then
 	else
 		echo "ALARM ALARM"
 		echo "ALARM, ALARM, the calculated gender (${sex}) and the gender given in the samplesheet(${Gender}) are not the same!"
-		echo -e "ALARM!\n For sample ${dedupBam%%.*} the calculated gender (${sex}) and the gender given in the samplesheet(${Gender}) are not the same!" > ${logDir}/${project}/pipeline.gendercheckfailed
+		echo -e "ALARM!\n For sample ${dedupBam%%.*} the calculated gender (${sex}) and the gender given in the samplesheet(${Gender}) are not the same!" > "${logDir}/${project}/pipeline.gendercheckfailed"
 	fi
 fi
 
