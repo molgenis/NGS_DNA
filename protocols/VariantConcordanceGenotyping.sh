8e6f2a086997ec3c3a0c4d6f271fd137ff713990
RoanKanninga <roankanninga@gmail.com>
2018-10-03 10:31:58 +0200

    added concordance check functionality to the pipeline (forcing calls on all array snps)

---
+++ protocols/VariantConcordanceGenotyping.sh
@@ -0,0 +1,65 @@
+#MOLGENIS walltime=23:59:00 mem=17gb ppn=2
+
+#Parameter mapping
+#string tmpName
+#string stage
+#string checkStage
+#string gatkVersion
+#string gatkJar
+#string tempDir
+#string intermediateDir
+#string indexFile
+#string project
+#string tmpDataDir
+#string logsDir
+#string groupname
+#string intervalListDir
+
+#Function to check if array contains value
+array_contains () { 
+    local array="$1[@]"
+    local seeking="${2}"
+    local in=1
+    for element in "${!array-}"; do
+        if [[ "${element}" == "${seeking}" ]]; then
+            in=0
+            break
+        fi
+    done
+    return "${in}"
+}
+
+if [[ ${intervalListDir} == *"ONCO_v"* ]]
+then
+
+	#Load GATK module
+	${stage} "${gatkVersion}"
+	${checkStage}
+
+	ALLGVCFs=()
+
+	for i in $(ls ${intermediateDir}*.variant.calls.g.vcf.gz)
+	do
+		array_contains ALLGVCFs "--variant ${i}" || ALLGVCFs+=("--variant ${i}")
+	done
+
+	gvcfSize=${#ALLGVCFs[@]}
+	if [ ${gvcfSize} -ne 0 ]
+	then
+	java -Xmx16g -XX:ParallelGCThreads=2 -Djava.io.tmpdir="${tempDir}" -jar \
+		"${EBROOTGATK}/${gatkJar}" \
+		-T GenotypeGVCFs \
+		-R "${indexFile}" \
+		-L ${intervalListDir}/GSA_SNPS.bed \
+		-allSites \
+		-o ${intermediateDir}/${project}.concordanceCheckSNPs.vcf \
+		${ALLGVCFs[@]} 
+
+	else
+		echo ""
+		echo "there is nothing to genotype, skipped"
+		echo ""
+	fi
+else
+	echo "this is not a ONCO run" 
+fi
