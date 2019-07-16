369b96e229330b2b7be0184d7ec6f60d219b6f75
medinatd <tylerdanmedina@gmail.com>
2019-07-16 17:21:00 +0200

    Modified inputs to un-split project VCFs.
    Originally combined all filtered per-sample snp and indel VCFs into one big project VCF. Now, filtered snp and indel VCFs are project-level, and not split into per-sample VCFs, to allow use of GATK4 MergeVcfs to simply combine the two (one snp, one indel) VCFs into one final VCF.

--- protocols/MergeIndelsAndSnpsProject.sh
+++ protocols/MergeIndelsAndSnpsProject.sh
@@ -10,38 +10,18 @@
 #string groupname
 #string intermediateDir
 #string	project
-
-#Function to check if array contains value
-array_contains () {
-    local array="$1[@]"
-    local seeking="${2}"
-    local in=1
-    for element in "${!array-}"; do
-        if [[ "${element}" == "${seeking}" ]]; then
-            in=0
-            break
-        fi
-    done
-    return "${in}"
-}
+#string projectVariantsSnpsOnlyFilteredVcf
+#string projectVariantsIndelsOnlyFilteredVcf
 
 #Load GATK module
 module load "${gatkVersion}"
 module list
 
-INPUTS=()
-for externalID in "${externalSampleID[@]}"
-do
-        array_contains INPUTS "--variant ${intermediateDir}/${externalID}.final.vcf" || INPUTS+=("--variant ${intermediateDir}/${externalID}.final.vcf")    # If bamFile does not exist in array add it
-done
-
-#merge all samples into one big vcf
-java -Xmx2g -jar "${EBROOTGATK}/${gatkJar}" \
--R "${indexFile}" \
--T CombineVariants \
-${INPUTS[@]} \
--o "${projectPrefix}.final.vcf.tmp"
+gatk --java-options "-Xmx3g" MergeVcfs \
+-I "${projectVariantsSnpsOnlyFilteredVcf}" \
+-I "${projectVariantsIndelsOnlyFilteredVcf}" \
+-D "${indexFileDictionary}" \
+-O "${projectPrefix}.final.vcf.tmp"
 
 echo "moving ${projectPrefix}.final.vcf.tmp to ${projectPrefix}.final.vcf"
 mv "${projectPrefix}.final.vcf.tmp" "${projectPrefix}.final.vcf"
-
