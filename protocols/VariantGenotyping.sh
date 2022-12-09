826994468a081f8a0f4e0efbb50967cc9b0d44b7
RoanKanninga <roankanninga@gmail.com>
2022-12-09 12:42:44 +0100

    fix

--- protocols/VariantGenotyping.sh
+++ protocols/VariantGenotyping.sh
@@ -15,18 +15,6 @@
 #string groupname
 
 #Function to check if array contains value
-array_contains () {
-	local array="$1[@]"
-	local seeking="${2}"
-	local in=1
-	for element in "${!array-}"; do
-		if [[ "${element}" == "${seeking}" ]]; then
-			in=0
-			break
-		fi
-	done
-	return "${in}"
-}
 
 makeTmpDir "${projectBatchGenotypedVariantCalls}"
 tmpProjectBatchGenotypedVariantCalls="${MC_tmpFile}"
