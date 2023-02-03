7b48ebd2dcf6167e1834d2f9e2887ae8bac4a37f
RoanKanninga <roankanninga@gmail.com>
2023-02-03 11:45:30 +0100

    fix workflow

--- test/protocols/Autotest.sh
+++ test/protocols/Autotest.sh
@@ -7,23 +7,21 @@
 #string intermediateDir
 
 module load ngs-utils
-
-rm -rf "/home/umcg-molgenis/NGS_DNA/output_${project}"
+homeFolder="/home/$(whoami)/NGS_DNA"
+rm -rf "${homeFolder}/output_${project}"
 count=0
 
 ### 1. Check if CoverageCalculations perBase output is still valid
-for i in PlatinumSample_NA12891
+for i in 'PlatinumSample_NA12891'
 do
 	diffCoveragePerTarget="false"
-	diff -q /home/umcg-molgenis/NGS_DNA/${i}.NGS_DNA_Test_v1.coveragePerTarget_True.txt ${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerTarget.txt || diffCoveragePerTarget="true"
-	if [ "${diffCoveragePerTarget}" == "true" ]
+	diff -q "${homeFolder}/${i}.NGS_DNA_Test_v1.coveragePerTarget_True.txt" "${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerTarget.txt" || diffCoveragePerTarget='true'
+	if [[ "${diffCoveragePerTarget}" == 'true' ]]
 	then
-		echo "there are differences in the CoveragePerTarget step between the test and the original output of ${i}"
-		echo "please fix the bug or update this test"
-		echo "diff -q /home/umcg-molgenis/NGS_DNA/${i}.NGS_DNA_Test_v1.coveragePerTarget_True.txt ${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerTarget.txt"
-                exit 1
-        else
-		echo "CoveragePerTarget is correct"
+		echo -e "there are differences in the CoveragePerTarget step between the test and the original output of ${i}\nplease fix the bug or update this test\ndiff -q ${homeFolder}/${i}.NGS_DNA_Test_v1.coveragePerTarget_True.txt ${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerTarget.txt"
+		exit 1
+	else
+		echo 'CoveragePerTarget is correct'
 	fi
 done
 
@@ -34,68 +32,56 @@ for i in PlatinumSample_NA12878
 do
 	head -50 "${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerBase.txt" > "${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerBase.selection.txt"
 	tail -50 "${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerBase.txt" >> "${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerBase.selection.txt"
-	diffCoveragePerBase="false"
+	diffCoveragePerBase='false'
 	diff -q /home/umcg-molgenis/NGS_DNA/${i}.NGS_DNA_Test_v1.coveragePerBase.selection_True.txt ${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerBase.selection.txt || diffCoveragePerBase="true"
-	if [ "${diffCoveragePerBase}" == "true" ]
+	if [[ "${diffCoveragePerBase}" == 'true' ]]
 	then
-		echo "there are differences in the CoveragePerBase step between the test and the original output of ${i}"
-		echo "please fix the bug or update this test"
-		echo "diff /home/umcg-molgenis/NGS_DNA/${i}.NGS_DNA_Test_v1.coveragePerBase.selection_True.txt ${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerBase.selection.txt"
+		echo -e "there are differences in the CoveragePerBase step between the test and the original output of ${i}\nplease fix the bug or update this test\ndiff ${homeFolder}/${i}.NGS_DNA_Test_v1.coveragePerBase.selection_True.txt ${intermediateDir}/${i}.NGS_DNA_Test_v1.coveragePerBase.selection.txt"
 		exit 1
 	else
 		echo "CoveragePerBase is correct"
 	fi
 
-	mantaDiff="$(zcat ${intermediateDir}/Manta/${i}//results/variants/real/diploidSV.vcf.gz | awk '{if ($1 !~ /#/){print $0}}')"
-	mantaDiffTrue="$(zcat /home/umcg-molgenis/NGS_DNA/${i}.Manta.diploidSV_True.vcf.gz | awk '{if ($1 !~ /#/){print $0}}')"
+	mantaDiff="$(zcat "${intermediateDir}/Manta/${i}//results/variants/real/diploidSV.vcf.gz" | awk '{if ($1 !~ /#/){print $0}}')"
+	mantaDiffTrue="$(zcat "${homeFolder}/${i}.Manta.diploidSV_True.vcf.gz" | awk '{if ($1 !~ /#/){print $0}}')"
 
-	if [ "${mantaDiff}" != "${mantaDiffTrue}" ]
+	if [[ "${mantaDiff}" != "${mantaDiffTrue}" ]]
 	then
-		echo "there are differences in the Manta step between the test and the original output of ${i}"
-                echo "please fix the bug or update this test"
-                echo "mantaOutput new run: ${mantaDiff}"
-		echo "mantaOutput True: ${mantaDiffTrue}"
-                exit 1
+		echo -e "there are differences in the Manta step between the test and the original output of ${i}\nplease fix the bug or update this test\nmantaOutput new run: ${mantaDiff}\nmantaOutput True: ${mantaDiffTrue}"
+		exit 1
 	else
-		echo "Manta output is correct"
+		echo 'Manta output is correct'
 	fi
 
-	if grep "RLV_PRESENT=FALSE" "${intermediateDir}/${i}.GAVIN.rlv.vcf"
+	if grep 'RLV_PRESENT=FALSE' "${intermediateDir}/${i}.GAVIN.rlv.vcf"
 	then
 		echo "RLV_PRESENT=FALSE found in ${intermediateDir}/${i}.GAVIN.rlv.vcf"
 	else
-
-		echo "there are differences in the Gavin output between the test and the original output of ${i} (it does NOT contain RLV_PRESENT=FALSE)"
-                echo "please fix the bug or update this test"
-                exit 1
+		echo -e "there are differences in the Gavin output between the test and the original output of ${i} (it does NOT contain RLV_PRESENT=FALSE)\nplease fix the bug or update this test"
+		exit 1
 	fi
-
 done
 
-
 ## 5. Check if the regular vcf's are still the same
-for i in "${project}" PlatinumSample_NA12878 PlatinumSample_NA12891
+for i in "${project}" 'PlatinumSample_NA12878' 'PlatinumSample_NA12891'
 do
 	mkdir -p "/home/umcg-molgenis/NGS_DNA/output_${project}/${i}"
-	${EBROOTNGSMINUTILS}/vcf-compare_2.0.sh -1 "/home/umcg-molgenis/NGS_DNA/${i}_True.final.vcf.gz" -2 "${projectResultsDir}/variants/${i}.final.vcf.gz" -o "/home/umcg-molgenis/NGS_DNA/output_${project}/${i}/"
+	${EBROOTNGSMINUTILS}/vcf-compare_2.0.sh -1 "${homeFolder}/${i}_True.final.vcf.gz" -2 "${projectResultsDir}/variants/${i}.final.vcf.gz" -o "${homeFolder}/output_${project}/${i}/"
 
-	if [[ -f "/home/umcg-molgenis/NGS_DNA/output_${project}/${i}/notInVcf1.txt" || -f "/home/umcg-molgenis/NGS_DNA/output_${project}/${i}/notInVcf2.txt" || -f "/home/umcg-molgenis/NGS_DNA/output_${project}/${i}/inconsistent.txt" ]]
+	if [[ -f "${homeFolder}/output_${project}/${i}/notInVcf1.txt" || -f "/home/umcg-molgenis/NGS_DNA/output_${project}/${i}/notInVcf2.txt" || -f "${homeFolder}/output_${project}/${i}/inconsistent.txt" ]]
 	then
-		echo "${i}: there are differences between the test and the original output"
-		echo "please fix the bug or update this test"
-		echo "the stats can be found here: /home/umcg-molgenis/NGS_DNA/output_${project}/${i}/vcfStats.txt"
+		echo -e "${i}: there are differences between the test and the original output\nplease fix the bug or update this test\nthe stats can be found here: ${homeFolder}/output_${project}/${i}/vcfStats.txt"
 		exit 1
 	else
 		echo "${i}: test succeeded"
-		head -2 "/home/umcg-molgenis/NGS_DNA/output_${project}/${i}/vcfStats.txt"
+		head -2 "${homeFolder}/output_${project}/${i}/vcfStats.txt"
 		count=$((count + 1))
 	fi
 done
 
-if [ "${count}" == "3" ]
+if [[ "${count}" == '3' ]]
 then
-	echo "Autotest complete"
-	echo "the statistics of the 3 tests can be found here: /home/umcg-molgenis/NGS_DNA/output_${project}/"
+	echo -e "Autotest complete\nthe statistics of the 3 tests can be found here: ${homeFolder}/output_${project}/"
 else
 	echo "interesting, this cannot be true, count is ${count}"
 fi
