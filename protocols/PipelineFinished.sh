0be6d472e2f4877f6a009bde3648cec69fff4fa7
RoanKanninga <roankanninga@gmail.com>
2023-06-02 14:01:32 +0200

    added copying samplesheet to resultsfolder + tiny fixes in processDragenTrendanalysis script

--- protocols/PipelineFinished.sh
+++ protocols/PipelineFinished.sh
@@ -2,12 +2,17 @@
 #string project
 #string logsDir
 #string runid
+#string projectJobsDir
+#string projectResultsDir
 
 set -e
 set -u
 set -o pipefail
 
-# Touch log file for GAP_Automated for starting copying project data to PRM
+
+# copy samplesheet to results directory
+rsync -a "${projectJobsDir}/${project}.csv" "${projectResultsDir}"
+
 
 if [[ -f "${logsDir}//${project}/${runid}.pipeline.started" ]]
 then
@@ -20,4 +25,4 @@ rm -f "${logsDir}/${project}/${runid}.pipeline.failed"
 
 echo "${logsDir}/${project}/${runid}.pipeline.finished is created"
 
-touch pipeline.finished
+touch 'pipeline.finished'
